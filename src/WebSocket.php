fe2c4fc4c28990f53b7ed2f0757d88cb60a78e51
risetechapps <apps@risetech.com.br>
2024-12-23 11:18:54 -0300

    Corrigido parametros.

--- src/WebSocket.php
+++ src/WebSocket.php
@@ -19,9 +19,7 @@ class WebSocket
     {
         try {
             $pusher = new Pusher($authKey, $secret, $appKey, self::$options);
-//            $pusher = new Pusher('243f551a-c8e8-4baf-a35e-ad2b369202d3', 'f974d9261f6308dd3d2cbd51e4ea9a36825f51cd', '460f9efb-c618-43b1-9aee-dec3f3f6bb8c', self::$options);
             $channels = $pusher->getChannels();
-
             $connections = config('broadcasting.connections');
 
             $connections['tenant'] = [
