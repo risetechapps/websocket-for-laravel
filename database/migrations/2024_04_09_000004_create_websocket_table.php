26fea893bd78212c45aa232a2f4127f39654d584
risetechapps <apps@risetech.com.br>
2025-03-21 12:58:45 -0300

    Atualizado migrations.

--- database/migrations/2024_04_09_000004_create_websocket_table.php
+++ database/migrations/2024_04_09_000004_create_websocket_table.php
@@ -12,25 +12,34 @@ return new class extends Migration {
     {
         Schema::create('websocket', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('key')->nullable();
-            $table->string('secret')->nullable();
+
+            $table->string('key', 255)->nullable();
+            $table->string('secret', 255)->nullable();
+
             $table->integer('max_connections')->default(1000);
             $table->boolean('enable_client_messages')->default(true);
             $table->boolean('enabled')->default(true);
             $table->integer('max_backend_events_per_sec')->default(100);
             $table->integer('max_client_events_per_sec')->default(100);
             $table->integer('max_read_req_per_sec')->default(100);
-            $table->json('webhooks')->nullable(); // JSON column para webhooks
-            $table->tinyInteger('max_presence_members_per_channel')->nullable()->default(100);
-            $table->tinyInteger('max_presence_member_size_in_kb')->nullable()->default(100);
-            $table->tinyInteger('max_channel_name_length')->nullable()->default(100);
-            $table->tinyInteger('max_event_channels_at_once')->nullable()->default(100);
-            $table->tinyInteger('max_event_name_length')->nullable()->default(100);
-            $table->tinyInteger('max_event_payload_in_kb')->nullable()->default(100);
-            $table->tinyInteger('max_event_batch_size')->nullable()->default(100);
+
+            $table->jsonb('webhooks')->nullable();
+
+            $table->tinyInteger('max_presence_members_per_channel')->default(100)->nullable();
+            $table->tinyInteger('max_presence_member_size_in_kb')->default(100)->nullable();
+            $table->tinyInteger('max_channel_name_length')->default(100)->nullable();
+            $table->tinyInteger('max_event_channels_at_once')->default(100)->nullable();
+            $table->tinyInteger('max_event_name_length')->default(100)->nullable();
+            $table->tinyInteger('max_event_payload_in_kb')->default(100)->nullable();
+            $table->tinyInteger('max_event_batch_size')->default(100)->nullable();
+
             $table->boolean('enable_user_authentication')->default(true);
+
             $table->timestamps();
             $table->softDeletes();
+
+            $table->index('key');
+            $table->index('enabled');
         });
     }
 
