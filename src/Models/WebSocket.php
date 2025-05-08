399baa7f0ed84ec0e98b08efe66cd8aef803fd60
risetechapps <apps@risetech.com.br>
2025-05-08 15:58:11 -0300

    Alterado namespace

--- src/Models/WebSocket.php
+++ src/Models/WebSocket.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace RiseTechApps\WebSocket\Model;
+namespace RiseTechApps\WebSocket\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
